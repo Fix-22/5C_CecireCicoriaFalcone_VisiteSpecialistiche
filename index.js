b6b0b0306d79acbf44b407ab2de0234863b6ad51
Simone <ceciresimone@itis-molinari.eu>
2024-10-28 18:11:24 +0100

    la chiusura del modal cancella datai in form

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import {generateReservationForm} from "./formComponent/formComponent.js";
 
 const modalBody = document.getElementById("modalBody");
+const clearFormButtons = document.querySelectorAll(".clearForm");
 
 const reservationForm = generateReservationForm(modalBody);
 reservationForm.build([8, 9, 10, 11, 12]);
 reservationForm.render();
 reservationForm.onsubmit(r => console.log(r));
+
+clearFormButtons.forEach(b => b.onclick = () => reservationForm.clear());
