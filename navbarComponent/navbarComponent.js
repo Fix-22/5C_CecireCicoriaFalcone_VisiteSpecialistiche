7f158825ec57b73ded741571e4b4b8f18e8b55c6
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-29 15:45:31 +0100

    miglioria grafica

--- navbarComponent/navbarComponent.js
+++ navbarComponent/navbarComponent.js
@@ -12,15 +12,22 @@ export const generateNavbar = (parentElement) => {
         render: () => {
             let html = '<div class="btn-group btn-group" data-toggle="buttons">';
 
-            html += configuration.map(e => '<label class="btn btn-outline-secondary"><input type="radio" name="options" autocomplete="off">' + e + "</label>").join("") + '</div>';
+            html += configuration.map(e => '<label class="btn btn-outline-secondary"' + 'id="label' + e + '">' + e + "</label>").join("") + '</div>';
 
             parentElement.innerHTML = html;
 
-            const radioButtons = document.querySelectorAll("input[name=options]");
-            radioButtons.forEach(b => {
-                b.onclick = () => {
-                    callback();
-                };
+            const labels = document.querySelectorAll("label.btn");
+            labels.forEach(l => {
+                l.onclick = () => {
+                    labels.forEach(e => {
+                        if (e.id !== l.id){
+                            e.classList.remove("active");
+                        }
+                        else {
+                            l.classList.add("active");
+                        }
+                    });
+                }
             });
         }
     };
