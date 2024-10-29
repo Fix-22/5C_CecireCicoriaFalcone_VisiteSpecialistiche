c6c49795ce91f26913dae4e131347bd90b9264ff
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-29 15:50:45 +0100

    miglioria grafica (2)

--- navbarComponent/navbarComponent.js
+++ navbarComponent/navbarComponent.js
@@ -11,12 +11,22 @@ export const generateNavbar = (parentElement) => {
         },
         render: () => {
             let html = '<div class="btn-group btn-group" data-toggle="buttons">';
+            let first = false;
 
-            html += configuration.map(e => '<label class="btn btn-outline-secondary"' + 'id="label' + e + '">' + e + "</label>").join("") + '</div>';
+            html += configuration.map(e => {
+                if (!first) {
+                    first = true;
+                    return '<label class="btn btn-outline-secondary active"' + 'id="label' + e + '">' + e + "</label>";
+                }
+                else {
+                    return '<label class="btn btn-outline-secondary"' + 'id="label' + e + '">' + e + "</label>";
+                }
+            }).join("") + '</div>';
 
             parentElement.innerHTML = html;
 
             const labels = document.querySelectorAll("label.btn");
+            
             labels.forEach(l => {
                 l.onclick = () => {
                     labels.forEach(e => {
