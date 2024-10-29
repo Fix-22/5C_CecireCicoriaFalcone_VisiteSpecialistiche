f37ff693ebc0ccc8df16bee01247814a0ef83cbf
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-10-29 13:30:17 +0100

    aggiunta fetch per prendere dati da conf.json

--- index.js
+++ index.js
@@ -2,9 +2,17 @@ import {generateReservationForm} from "./formComponent/formComponent.js";
 
 const modalBody = document.getElementById("modalBody");
 const clearFormButtons = document.querySelectorAll(".clearForm");
+let confFileContent;
+const hours = [8, 9, 10, 11, 12];
+
+fetch("./conf.json")
+.then(r => r.json())
+.then(data => {
+    confFileContent = data;
+});
 
 const reservationForm = generateReservationForm(modalBody);
-reservationForm.build([8, 9, 10, 11, 12]);
+reservationForm.build(hours);
 reservationForm.render();
 reservationForm.onsubmit(r => console.log(r));
 
